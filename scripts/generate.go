0440b74bd29ff20287ccf373637c093677c8360b
adibrastegarnia <arastega@purdue.edu>
2019-03-31 11:37:03 -0400

    minor changes in generate.go script

--- scripts/generate.go
+++ scripts/generate.go
@@ -6,7 +6,7 @@ import (
 )
 
 
-//go:generate go run src/github.com/openconfig/ygot/proto_generator/protogenerator.go  -generate_fakeroot -base_import_path="../pkg/proto"  -path=../yang -output_dir=../pkg/proto    -package_hierarchy   -package_name=topology -enum_package_name=enums ../yang/ietf-network-topology.yang 
+//go:generate go run src/github.com/openconfig/ygot/proto_generator/protogenerator.go  -generate_fakeroot -base_import_path="../pkg/proto"  -path=../yang -output_dir=../pkg/proto  -fakeroot_name=network-topology  -package_hierarchy   -package_name=topology -enum_package_name=enums ../yang/ietf-network-topology.yang 
 
 func main() {
 	
