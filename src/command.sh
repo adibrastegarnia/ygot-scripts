ae33afa9c64aff4305146fba0c52bc7f24896a20
adibrastegarnia <arastega@purdue.edu>
2019-04-01 19:25:07 -0400

    protobuf rules have been updated to generate go files for the network topology

--- src/command.sh
+++ src/command.sh
@@ -2,4 +2,4 @@
 
 go get ./...
 
-go run github.com/openconfig/ygot/proto_generator/protogenerator.go  -generate_fakeroot  -base_import_path="./models/proto"  -path=./yang  -output_dir=./models/proto  -fakeroot_name=network-topology  -package_hierarchy    -package_name=topology  -enum_package_name=enums ./yang/ietf-network-topology.yang   
+go run github.com/openconfig/ygot/proto_generator/protogenerator.go  -generate_fakeroot  -base_import_path="./models/proto"  -path=./yang  -output_dir=./models/proto  -fakeroot_name=network-topology  -package_hierarchy    -package_name=topology  -enum_package_name=enums ./yang/ietf-network-topology.yang  
