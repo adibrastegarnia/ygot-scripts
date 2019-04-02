8b365514c51be7f1a94d27bbcdccb8012a4c2a6b
adibrastegarnia <arastega@purdue.edu>
2019-04-01 22:47:09 -0400

    the script has been updated to generate go structures for the yang files

--- src/command.sh
+++ src/command.sh
@@ -2,4 +2,5 @@
 
 go get ./...
 
-go run github.com/openconfig/ygot/proto_generator/protogenerator.go  -generate_fakeroot  -base_import_path="./models/proto"  -path=./yang  -output_dir=./models/proto  -fakeroot_name=network-topology  -package_hierarchy    -package_name=topology  -enum_package_name=enums ./yang/ietf-network-topology.yang  
+go run github.com/openconfig/ygot/generator/generator.go -path=yang -output_file=pkg/topo/topo.go -package_name=topo -generate_fakeroot -fakeroot_name=topology yang/ietf-network.yang yang/ietf-network-topology.yang
+go run github.com/openconfig/ygot/proto_generator/protogenerator.go  -generate_fakeroot  -base_import_path="./models/proto"  -path=./yang  -output_dir=./models/proto  -fakeroot_name=topology  -package_hierarchy    -package_name=topo  -enum_package_name=enums ./yang/ietf-network-topology.yang  
